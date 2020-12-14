8bdd08bafaea95db10393681a76d726b978ee7f8
Xitiz Verma <xitizverma@gmail.com>
2020-12-14 23:07:08 +0530

    Added SBI

---
+++ SBI.java
@@ -0,0 +1,30 @@
+
+class SBI implements Bank
+{
+	String name;
+	double principal;
+	double period;
+	double interest;
+	double roi = 8.5;
+	
+	public void getDetails ()
+	{
+		System.out.println ("Enter Name");
+		name = sc.nextLine();
+		System.out.println ("Enter Principal");
+		principal = sc.nextDouble();
+		System.out.println ("Enter Period");
+		period = sc.nextDouble();
+	}
+	
+	public void calculateInterest ()
+	{
+		interest = (principal * period * roi)/100;
+	}
+	
+	public void getMAmount ()
+	{
+		double mAmount = principal + interest;
+		System.out.println (name+" your Maturity Amount in SBI is Rs. "+mAmount+"\n");
+	}
+}
